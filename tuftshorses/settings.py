53d30d14a141a5ecbd66fb4a036f51b6b80eb9a8
benweitzman <benjamin.weitzman@tufts.edu>
2012-11-14 17:02:45 -0500

    remove postgres config

--- tuftshorses/settings.py
+++ tuftshorses/settings.py
@@ -22,9 +22,6 @@ DATABASES = {
     }
 }
 
-if not bool(os.environ.get('LOCAL_DEV', False)):
-    DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
