9188c98d738a01d516f938c02899a497c7948a7a
=David Eder <=david.eder@tufts.edu>
2013-01-30 18:56:03 -0500

    my changes from yesterday

--- scheduler/models.py
+++ scheduler/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from horseshow.models import Rider, Ring
 
 class Schedule(models.Model):
-    length = models.IntegerField()
-    breakLength = models.IntegerField()
-    timeSlots = models.ManyToManyField('TimeSlot')
-    ring = models.OneToOneField(Ring)
+  length = models.IntegerField()
+  breakLength = models.IntegerField()
+  timeSlots = models.ManyToManyField('TimeSlot')
+  ring = models.OneToOneField(Ring)
 
 
 class TimeSlot(models.Model):
-	startTime = models.DateTimeField()
-	endTime = models.DateTimeField()
-	riders = models.ManyToManyField(Rider)
+  startTime = models.DateTimeField()
+  endTime = models.DateTimeField()
+  riders = models.ManyToManyField(Rider)
