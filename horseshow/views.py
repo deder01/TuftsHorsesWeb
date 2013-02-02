7956d369508c76bb663851755ce1769d810a8fa5
David Eder <davideder26@gmail.com>
2013-02-02 16:33:15 -0500

    added login form, and login capabilites

--- horseshow/views.py
+++ horseshow/views.py
@@ -6,15 +6,29 @@ from django.template.context import RequestContext
 from django.contrib.auth import authenticate, login, logout
 from models import *
 
+def user_login(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+      login(request, user)
+  return redirect('horseshow.views.home')
+
+def user_logout(request):
+  logout(request)
+  return redirect('horseshow.views.home')
+
 def home(request):
-  team = Team.objects.all()[0]
+  if request.user.is_authenticated() == False:
+    return render_to_response('login.hamlpy', context_instance=RequestContext(request, {}))
+  team = request.user.riderTeam.all()[0]
   return render_to_response('index.hamlpy', context_instance=RequestContext(request, {
                                               'team':team,
-                                              'teamname':teamname,
                                              }))
 
 def team(request, teamid):
-  team = Team.objects.all().filter(id=teamid)[0]
+  team = request.user.riderTeam.all()[0]
   teamname = team.school.lower()
   return render_to_response('teams/'+teamname+'/index.hamlpy',
                             context_instance=RequestContext(request, {
@@ -23,8 +37,10 @@ def team(request, teamid):
                               }))
 
 def show(request, showid):
+  team = Team.objects.all().filter(id=teamid)[0]
   show = HorseShow.objects.all().filter(id=showid)[0]
   return render_to_response('shows/'+show.title.lower()+'/index.hamlpy',
                             context_instance=RequestContext(request, {
+                              'team':team,
                               'show':show,
                               }))
