3ea857d1e6551c0a1d01ceefa6de67a416825bc4
David Eder <davideder26@gmail.com>
2013-02-04 10:54:36 -0500

    updated staticfiles_dirs for collecstatic

--- tuftshorses/settings.py
+++ tuftshorses/settings.py
@@ -4,10 +4,6 @@ import dj_database_url
 import middleware
 from re import compile
 
- 
- 
-
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -64,6 +60,7 @@ STATIC_URL = os.getenv('STATIC_URL', 'http://s3.amazonaws.com/tuftshorses/')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(STATIC_ROOT, "css/"),
     #os.path.join(PROJECT_ROOT_PATH,"static/"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
